f9778591501c9a45b1231d7fe401093963c8eabc
Julius de Bruijn <julius@nauk.io>
2023-11-29 14:47:35 +0100

    pg2

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,11 @@
+import { g, config, connector } from "@grafbase/sdk";
+
+const pg = connector.Postgres("pg", {
+  url: g.env("CONNECTION_STRING"),
+});
+
+g.datasource(pg);
+
+export default config({
+  schema: g,
+});
