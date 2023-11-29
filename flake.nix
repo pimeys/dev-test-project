2dfae96ab2eefb2c898bcbb73bff920d0b1361ba
Julius de Bruijn <julius@nauk.io>
2023-11-29 14:56:21 +0100

    remove grafbase dir

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Julius Test project";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/master";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    devshell,
+    nixpkgs,
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        devshell.flakeModule
+      ];
+
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devshells.default = {
+          commands = [
+            {
+              package = pkgs.nodejs;
+              category = "development";
+            }
+            {
+              package = pkgs.nodePackages.npm;
+              category = "development";
+            }
+            {
+              package = pkgs.rover;
+              category = "development";
+            }
+          ];
+        };
+      };
+    };
+}
